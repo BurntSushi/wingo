d38a746c0e56bd1d0a511932f3375292795b3e7c
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-06-24 21:59:29 -0400

    Gribble parser is pretty much complete.

--- gribble/parser.go
+++ gribble/parser.go
@@ -2,84 +2,119 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"strconv"
 	"strings"
 	"text/scanner"
 )
 
+// parser maintains the state of the Gribble parser. It embeds a Scanner value
+// and keeps track of the current token and token text (since Peek/Next always
+// look at the *next* token, which isn't what we want typically when reporting
+// errors about the current token).
+//
+// errors is a slice of all errors encountered during parsing. Parsing never
+// stops because of an error.
 type parser struct {
 	*scanner.Scanner
-	tok rune
-	tokText string
-	errors []error
+	original string
+	verbose  bool
+	tok      rune
+	tokText  string
+	errors   []error
 }
 
+// command represents a Gribble command invocation; which is simply a name and 
+// a list of arguments.
 type command struct {
-	name string
+	name   string
 	params []param
 }
 
+// String returns a recursively formatted command, Lisp style.
 func (cmd *command) String() string {
+	if len(cmd.params) == 0 {
+		return fmt.Sprintf("(%s)", cmd.name)
+	}
+
 	params := make([]string, len(cmd.params))
 	for i, param := range cmd.params {
-		params[i] = fmt.Sprintf("%s", param)
+		switch concrete := param.(type) {
+		case int:
+			params[i] = fmt.Sprintf("%d", concrete)
+		case float64:
+			params[i] = fmt.Sprintf("%0.2f", concrete)
+		case string:
+			params[i] = concrete
+		case *command:
+			params[i] = concrete.String()
+		default:
+			panic(fmt.Sprintf("Unexpected type: %T", concrete))
+		}
 	}
 	return fmt.Sprintf("(%s %s)", cmd.name, strings.Join(params, " "))
 }
 
 // param is either a string, int, float64 or a command.
-type param interface {}
+// It might be wise to actually make special int, float64, and string types,
+// but that seems a bit much at the moment. (Their only common function would
+// be to return a String method, which is only used for debugging purposes
+// anyway.)
+type param interface{}
 
-func parse(reader io.Reader) (*command, error) {
-	p := newParser(reader)
+// parse takes a command invocation string and returns its a command value
+// representing it. Note that even when an error is returned, the command
+// value is also returned since parsing doesn't stop on an error.
+func parse(invocation string, verbose bool) (*command, error) {
+	p := newParser(invocation, verbose)
 	cmd := p.command()
 	if len(p.errors) == 0 && p.tok != scanner.EOF {
-		p.error("EOF")
+		p.parseError("EOF")
 	}
 	if len(p.errors) > 0 {
 		reterrs := make([]string, len(p.errors))
 		for i, err := range p.errors {
 			reterrs[i] = err.Error()
 		}
-		return nil, fmt.Errorf(strings.Join(reterrs, "\n"))
+		return cmd, fmt.Errorf(strings.Join(reterrs, "\n"))
 	}
 	return cmd, nil
 }
 
-func newParser(reader io.Reader) *parser {
+// newParser is a parser constructor and also initializes the Scanner.
+func newParser(invocation string, verbose bool) *parser {
 	p := &parser{
-		Scanner: &scanner.Scanner{},
+		Scanner:  &scanner.Scanner{},
+		original: invocation,
+		verbose:  verbose,
+		errors:   make([]error, 0),
 	}
-	p.Init(reader)
+	p.Init(strings.NewReader(invocation))
+	p.Error = func(s *scanner.Scanner, msg string) {
+		p.errors = append(p.errors, p.error(s.Position, msg))
+	}
 	return p
 }
 
+// Scan intercepts all Scan requests to update tok and tokText state.
 func (p *parser) Scan() rune {
 	r := p.Scanner.Scan()
 	p.tok, p.tokText = r, p.TokenText()
 	return r
 }
 
-func (p *parser) error(kind string) {
-	var found string
-	if p.tok < 0 {
-		found = fmt.Sprintf("'%s' (%s)",
-			p.tokText, scanner.TokenString(p.tok))
-	} else {
-		found = fmt.Sprintf("'%s'", p.tokText)
-	}
-	p.errors = append(p.errors,
-		fmt.Errorf("Line %d, Column %d: Expected '%s' but found %s.",
-			p.Position.Line, p.Position.Column, kind, found))
-}
-
+// demands checks the current token for equality with 'c'. If they are equal,
+// the scanner progresses unhindered. Otherwise, an error is logged and the
+// scanner still progressed.
 func (p *parser) demands(c rune) {
 	if p.tok != c {
-		p.error(string([]rune{c}))
+		p.parseError(string([]rune{c}))
 	}
 	p.Scan()
 }
 
+// command parses a command invocation. It can handle arbitrarily nested
+// parantheses. An error is logged when something other than a scanner.Ident
+// or a '(' is found.
 func (p *parser) command() *command {
 	switch tok := p.Scan(); tok {
 	case '(':
@@ -88,15 +123,19 @@ func (p *parser) command() *command {
 		return cmd
 	case scanner.Ident:
 		cmd := &command{
-			name: p.TokenText(),
+			name:   p.TokenText(),
 			params: p.params(),
 		}
 		return cmd
 	}
-	p.error("command")
+	p.parseError("command")
 	return nil
 }
 
+// params parses a list of parameters to a command invocation. A parameter
+// can be either a string, an integer, a float or a command (surrounded by
+// parantheses). If a ')' or an EOF is found, parameter parsing stops.
+// If anything else is found, an error is reported.
 func (p *parser) params() []param {
 	params := make([]param, 0, 4)
 	tok := p.Scan()
@@ -107,25 +146,36 @@ func (p *parser) params() []param {
 		case scanner.String:
 			params = append(params, p.TokenText())
 		case scanner.Int:
-			params = append(params, p.TokenText())
+			if n, err := strconv.ParseInt(p.TokenText(), 0, 32); err != nil {
+				p.parseError("integer")
+				params = append(params, nil)
+			} else {
+				params = append(params, int(n))
+			}
 		case scanner.Float:
-			params = append(params, p.TokenText())
-		default:
+			if n, err := strconv.ParseFloat(p.TokenText(), 64); err != nil {
+				p.parseError("float")
+				params = append(params, nil)
+			} else {
+				params = append(params, float64(n))
+			}
+		case ')':
 			return params
+		default:
+			p.parseError("parameter")
 		}
 		tok = p.Scan()
 	}
 	return params
 }
 
+// temporary. for testing/debugging only.
 func main() {
 	// cmd := `(Workspace '\p50' (Move 5.0))` 
-	cmd := `(Workspace (Move 5.0))`
-	c, err := parse(strings.NewReader(cmd))
+	cmd := `(Workspace (Move 5 (Refresh)))`
+	c, err := parse(cmd, true)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
 	fmt.Println(c)
 }
-
---
+++ gribble/parser_error.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"text/scanner"
+)
+
+// error returns either a verbose or a simple error, depending upon the value
+// of 'verbose' in the parser state.
+func (p *parser) error(pos scanner.Position, msg string) error {
+	if p.verbose {
+		return p.verboseError(pos, msg)
+	}
+	return p.simpleError(pos, msg)
+}
+
+// simpleError takes a position and returns a string with the line and column
+// number, along with the error message.
+func (p *parser) simpleError(pos scanner.Position, msg string) error {
+	return fmt.Errorf("Line %d, Column %d: %s", pos.Line, pos.Column, msg)
+}
+
+// verboseError takes a position and returns a string with the line that 
+// contains that position with a '^' symbol placed beneathe the column 
+// indicated by that position.
+func (p *parser) verboseError(pos scanner.Position, msg string) error {
+	// Sanitize the data first so that we can split on '\n' reliably.
+	sanitized := strings.TrimSpace(p.original)
+	sanitized = strings.Replace(sanitized, "\t", " ", -1)
+	sanitized = strings.Replace(sanitized, "\r\n", "\n", -1)
+	sanitized = strings.Replace(sanitized, "\r", "\n", -1)
+	lines := strings.Split(sanitized, "\n")
+	linei, coli := pos.Line-1, pos.Column-1
+
+	if linei < 0 || linei >= len(lines) {
+		panic(fmt.Sprintf("Line number %d is out of bounds.", linei))
+	}
+	line := lines[linei]
+
+	// coli can point to one character after the last in 'line' (for EOF).
+	if coli < 0 || coli > len(line) {
+		panic(fmt.Sprintf("Column number %d is out of bounds.", coli))
+	}
+
+	caret := make([]rune, len(line)+1)
+	for i := 0; i <= len(line); i++ {
+		if i == coli {
+			caret[i] = '^'
+		} else {
+			caret[i] = ' '
+		}
+	}
+	return fmt.Errorf("Line %d, Column %d: %s\n%s",
+		pos.Line, pos.Column, msg, line+"\n"+string(caret)+"\n")
+}
+
+// parseError can be called at any point during parsing and an error will be
+// appeneded to the parser's error list.
+//
+// 'kind' is the string representation of the value that was expected (but
+// obviously wasn't found).
+func (p *parser) parseError(kind string) {
+	var found string
+	if p.tok < 0 {
+		found = fmt.Sprintf("'%s' (%s)",
+			p.tokText, scanner.TokenString(p.tok))
+	} else {
+		found = fmt.Sprintf("'%s'", p.tokText)
+	}
+
+	err := p.error(p.Position,
+		fmt.Sprintf("Expected '%s' but found %s.", kind, found))
+	p.errors = append(p.errors, err)
+}
