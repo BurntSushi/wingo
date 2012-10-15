24825f992ca996946d0a8c64b7d7c2e8286b63ef
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-10-15 03:57:10 -0400

    Clean up and documentation.

--- wingo-cmd/main.go
+++ wingo-cmd/main.go
@@ -45,6 +45,79 @@ func init() {
 }
 
 func main() {
+	// If a list/usage flag is set, print the stuff and exit.
+	handleFlags()
+
+	// Get the commands from file/stdin/argument.
+	cmds := getCommands()
+
+	// Connect to the Wingo command server.
+	conn, err := net.Dial("unix", path.Join(os.TempDir(), "wingo-ipc"))
+	if err != nil {
+		log.Fatalf("Could not connect to Wingo IPC: %s", err)
+	}
+
+	// Send the command.
+	if _, err = fmt.Fprintf(conn, "%s%c", cmds, 0); err != nil {
+		log.Fatalf("Error writing command: %s", err)
+	}
+
+	// Read the response.
+	reader := bufio.NewReader(conn)
+	msg, err := reader.ReadString(0)
+	if err != nil {
+		log.Fatalf("Could not read response: %s", err)
+	}
+	msg = msg[:len(msg)-1] // get rid of null terminator
+
+	fmt.Println(msg)
+}
+
+// getCommands inspects the arguments to extra a series of commands. If the
+// '-f' flag is set, then the commands are read from a file or stdin ("-").
+// Otherwise, the command is read from the first command line argument.
+func getCommands() (cmds string) {
+	// If '-f' is set, use commands from the file specified.
+	// Otherwise, make sure there is one and only one argument (the command).
+	if len(flagFileInput) > 0 {
+		var contents []byte
+		var err error
+
+		if flagFileInput == "-" { // stdin
+			contents, err = ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				log.Fatalf("Could not read stdin: %s", err)
+			}
+		} else {
+			contents, err = ioutil.ReadFile(flagFileInput)
+			if err != nil {
+				log.Fatalf("Could not read file '%s': %s", flagFileInput, err)
+			}
+		}
+
+		// Ignore any line starting with '#' after trimming.
+		lines := make([]string, 0)
+		for _, line := range bytes.Split(contents, []byte{'\n'}) {
+			line = bytes.TrimSpace(line)
+			if len(line) == 0 || line[0] == '#' {
+				continue
+			}
+			lines = append(lines, string(line))
+		}
+		cmds = strings.Join(lines, "\n")
+	} else {
+		if flag.NArg() != 1 {
+			log.Printf("Expected 1 argument but got %d arguments.", flag.NArg())
+			usage()
+		}
+		cmds = flag.Arg(0)
+	}
+	return
+}
+
+// handleFlags inspects the list/usage flags, and will print the appropriate
+// output and exit with success if one of them is set.
+func handleFlags() {
 	switch {
 	case flagListCommands:
 		fmt.Println(commands.Env.String())
@@ -74,60 +147,6 @@ func main() {
 		fmt.Printf("\t%s\n", strings.Replace(help, "\n", "\n\t", -1))
 		os.Exit(0)
 	}
-
-	// If '-f' is set, use commands from the file specified.
-	// Otherwise, make sure there is one and only one argument (the command).
-	var cmds string
-	var err error
-	if len(flagFileInput) > 0 {
-		var contents []byte
-		if flagFileInput == "-" {
-			contents, err = ioutil.ReadAll(os.Stdin)
-			if err != nil {
-				log.Fatalf("Could not read stdin: %s", err)
-			}
-		} else {
-			contents, err = ioutil.ReadFile(flagFileInput)
-			if err != nil {
-				log.Fatalf("Could not read file '%s': %s", flagFileInput, err)
-			}
-		}
-
-		// Ignore any line starting with '#' after trimming.
-		lines := make([]string, 0)
-		for _, line := range bytes.Split(contents, []byte{'\n'}) {
-			line = bytes.TrimSpace(line)
-			if len(line) == 0 || line[0] == '#' {
-				continue
-			}
-			lines = append(lines, string(line))
-		}
-		cmds = strings.Join(lines, "\n")
-	} else {
-		if flag.NArg() != 1 {
-			log.Printf("Expected 1 argument but got %d arguments.", flag.NArg())
-			usage()
-		}
-		cmds = flag.Arg(0)
-	}
-
-	conn, err := net.Dial("unix", path.Join(os.TempDir(), "wingo-ipc"))
-	if err != nil {
-		log.Fatalf("Could not connect to Wingo IPC: %s", err)
-	}
-
-	if _, err = fmt.Fprintf(conn, "%s%c", cmds, 0); err != nil {
-		log.Fatalf("Error writing command: %s", err)
-	}
-
-	reader := bufio.NewReader(conn)
-	msg, err := reader.ReadString(0)
-	if err != nil {
-		log.Fatalf("Could not read response: %s", err)
-	}
-	msg = msg[:len(msg)-1] // get rid of null terminator
-
-	fmt.Println(msg)
 }
 
 func usage() {
