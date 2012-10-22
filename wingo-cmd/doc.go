30721a3feddf3a0dcda3679bc7b7f31aab207f15
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-10-21 23:23:11 -0400

    tabs grrr

--- wingo-cmd/doc.go
+++ wingo-cmd/doc.go
@@ -16,18 +16,18 @@ Use this instead:
 	wingo-cmd 'AddWorkspace "new-workspace-name"'
 
 The flags are:
-    -f file
+	-f file
 		When set, commands will be read from the specified file. The file
 		may contain multiple commands, where each is one its own line and
 		enclosed with '(' and ')'.
 		If "-" is used, commands will be read from stdin.
 	--list
 		List all commands and the names of each parameter for each command.
-    --list-types
+	--list-types
 		The same as '--list', but includes the types of each parameter.
 	--list-usage
 		The same as '--list-types', but includes a description of the command.
-    --usage command-name
+	--usage command-name
 		Get the usage information for the command named "command-name".
 	--poll milliseconds
 		When milliseconds is greater than 0, the given commands will be
